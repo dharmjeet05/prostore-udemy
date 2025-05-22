a65e0310f1d1358f1a0a7e6ea66a9d4046ad032f
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2025-05-22 15:31:11 +0530

    Loading, Not Found and Menu DONE

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Loader from '@/assets/loader.gif'
+
+const LoadingPage = () => {
+  return ( <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100wh'
+  }}>
+    <Image src={Loader} height={150} width={150} alt="Loading..." />
+  </div> );
+}
+ 
+export default LoadingPage;
